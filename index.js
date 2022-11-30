189a23018d5a07b19066dddeb77927b7afa79c11
What-Question-Mark <97435840+What-Question-Mark@users.noreply.github.com>
2022-11-30 16:03:52 +1000

    User pages

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const fs = require('fs');
+const users = require('./api/getuser.js');
 
 app.use(express.static('public'))
 
@@ -13,6 +13,9 @@ app.get('/settings', (req, res) => {res.sendFile('settings/index.html', {root: p
 app.get('/login', (req, res) => {res.sendFile('auth/login.html', {root: path.join(__dirname, 'public')});});
 app.get('/signup', (req, res) => {res.sendFile('auth/signup.html', {root: path.join(__dirname, 'public')});});
 
+for (let i = 0; i < users.length; i++) {
+    app.get('/' + users[i].username, (req, res) => {res.sendFile('user/index.html', {root: path.join(__dirname, 'public')});});
+}
 
 app.listen(process.env.PORT || 3000);
 module.exports = app;
