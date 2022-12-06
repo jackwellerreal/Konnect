16c7035a6e11a90b011ffc89ea00fedbda9d8871
What-Question-Mark <97435840+What-Question-Mark@users.noreply.github.com>
2022-12-06 14:39:45 +1000

    Done some stuff 🦺

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const path = require('path');
 require('dotenv').config()
@@ -15,4 +16,4 @@ app.get('/settings', (req, res) => {res.sendFile('settings/index.html', {root: p
 app.get('/login', (req, res) => {res.sendFile('auth/login.html', {root: path.join(__dirname, 'public')});});
 app.get('/signup', (req, res) => {res.sendFile('auth/signup.html', {root: path.join(__dirname, 'public')});});
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000 , () => console.log('Server started on port 3000: http://localhost:3000'));
