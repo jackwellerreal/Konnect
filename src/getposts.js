ff80030b1faca847945da250cab8050bcce340df
What-Question-Mark <97435840+What-Question-Mark@users.noreply.github.com>
2022-12-08 15:46:41 +1000

    Added expressjs :computer:

---
+++ src/getposts.js
@@ -0,0 +1,27 @@
+const validateApiKey = require('./validateapikey.js');
+const moment = require('moment');
+require('dotenv').config()
+
+const { MongoClient } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
+
+async function getPosts(apiKey) {
+    if (validateApiKey(apiKey)) {
+        await client.connect();
+        const db = client.db('konnect');
+        const collection = db.collection('posts');
+        
+        var posts = [];
+        for await (let post of collection.find()) {
+            posts.push(post);
+        }
+
+        await client.close();
+        return posts;
+    } else {
+        return "Invalid API key";
+    }
+}
+
+module.exports = { getPosts };
