d92e72f253ea526c871104515d95b2f01c5f21de
What-Question-Mark <97435840+What-Question-Mark@users.noreply.github.com>
2022-12-11 20:26:56 +1000

    footer 🦶

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ const app = express();
 const path = require('path');
 require('dotenv').config()
 
-// Pages
-
 app.use(express.static('public'))
+
+app.get('/', (req, res) => {res.sendFile('home/index.html', {root: path.join(__dirname, 'public')});});
 
 app.get('/app', (req, res) => {res.sendFile('app/index.html', {root: path.join(__dirname, 'public')});});
 app.get('/app/news', (req, res) => {res.sendFile('app/news/index.html', {root: path.join(__dirname, 'public')});});
 app.get('/app/messages', (req, res) => {res.sendFile('app/messages/index.html', {root: path.join(__dirname, 'public')});});
 app.get('/app/themes', (req, res) => {res.sendFile('app/themes/index.html', {root: path.join(__dirname, 'public')});});
 app.get('/app/settings', (req, res) => {res.sendFile('app/settings/index.html', {root: path.join(__dirname, 'public')});});
+
 app.get('/login', (req, res) => {res.sendFile('auth/login.html', {root: path.join(__dirname, 'public')});});
 app.get('/signup', (req, res) => {res.sendFile('auth/signup.html', {root: path.join(__dirname, 'public')});});
 
