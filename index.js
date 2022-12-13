5cabfcf8c167fda2bb17dc36eb282b32e9d7fa16
What-Question-Mark <97435840+What-Question-Mark@users.noreply.github.com>
2022-12-13 10:25:20 +1000

    Beta 🛒

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const axios = require('axios');
-const app = express();
 const path = require('path');
+
+const app = express();
 require('dotenv').config()
 
 app.use(express.static(__dirname + '/public'));
@@ -15,22 +16,5 @@ app.get('/about/branding', (req, res) => {res.sendFile('home/about/branding/inde
 app.get('/about/acknowledgements', (req, res) => {res.sendFile('home/about/acknowledgements/index.html', {root: path.join(__dirname, 'public')});});
 app.get('/support', (req, res) => {res.sendFile('home/support/index.html', {root: path.join(__dirname, 'public')});});
 app.get('/beta', (req, res) => {res.sendFile('home/about/beta/index.html', {root: path.join(__dirname, 'public')});});
-
-/*
-
-App
-
-app.get('/app', (req, res) => {res.sendFile('app/index.html', {root: path.join(__dirname, 'public')});});
-app.get('/app/news', (req, res) => {res.sendFile('app/news/index.html', {root: path.join(__dirname, 'public')});});
-app.get('/app/messages', (req, res) => {res.sendFile('app/messages/index.html', {root: path.join(__dirname, 'public')});});
-app.get('/app/themes', (req, res) => {res.sendFile('app/themes/index.html', {root: path.join(__dirname, 'public')});});
-app.get('/app/settings', (req, res) => {res.sendFile('app/settings/index.html', {root: path.join(__dirname, 'public')});});
-
-Auth
-
-app.get('/login', (req, res) => {res.sendFile('auth/login.html', {root: path.join(__dirname, 'public')});});
-app.get('/signup', (req, res) => {res.sendFile('auth/signup.html', {root: path.join(__dirname, 'public')});});
-
-*/
 
 app.listen(process.env.PORT || 3000 , () => console.log('Server started on port 3000: http://localhost:3000'));
